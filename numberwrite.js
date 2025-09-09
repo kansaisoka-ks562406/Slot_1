5b8f797fd8b82b49a30a82a157d160143a3e1370
kansaisoka.ks562406 <ks562406@s.kansai.soka.ed.jp>
2025-09-09 12:03:06 +0900

    Update numberwrite.js

--- numberwrite.js
+++ numberwrite.js
@@ -1,6 +1,20 @@
-function slotwrite(){
-  div1.innerHTML = "<h2></h2>" 
+function slotwrite() {
+  function div1_write(output, h_id) {
+    if (h_id === 1) {
+      div1.innerHTML = "<h1>" + output + "</h1>";
+    } else if (h_id === 2) {
+      div1.innerHTML = "<h2>" + output + "</h2>";
+    } else if (h_id === 3) {
+      div1.innerHTML = "<h3>" + output + "</h3>";
+    } else if (h_id === 4) {
+      div1.innerHTML = "<h4>" + output + "</h4>";
+    } else if (h_id === 5) {
+      div1.innerHTML = "<h5>" + output + "</h5>";
+    } else {
+      div1.innerHTML = "<h6>" + output + "</h6>";
+    }
+  }
+
+  // ここで呼び出せる
+  // div1_write("テスト出力", 3);
 }
-
-
-//div1.innerHTML = " " ;
