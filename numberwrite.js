2b4d408a6ed85dbb4d17423e59332d30547fd4b9
kansaisoka.ks562406 <ks562406@s.kansai.soka.ed.jp>
2025-09-09 13:42:33 +0900

    Update numberwrite.js

--- numberwrite.js
+++ numberwrite.js
@@ -1,20 +1,13 @@
 function slotwrite() {
   function div1_write(output, h_id) {
-    if (h_id === 1) {
-      div1.innerHTML = "<h1>" + output + "</h1>";
-    } else if (h_id === 2) {
-      div1.innerHTML = "<h2>" + output + "</h2>";
-    } else if (h_id === 3) {
-      div1.innerHTML = "<h3>" + output + "</h3>";
-    } else if (h_id === 4) {
-      div1.innerHTML = "<h4>" + output + "</h4>";
-    } else if (h_id === 5) {
-      div1.innerHTML = "<h5>" + output + "</h5>";
-    } else {
-      div1.innerHTML = "<h6>" + output + "</h6>";
-    }
+    if (h_id < 1 || h_id > 6) h_id = 6; // 範囲外なら h6 に
+    div1.innerHTML = "<h" + h_id + ">" + output + "</h" + h_id + ">";
   }
-
-  // ここで呼び出せる
-  // div1_write("テスト出力", 3);
+  function speed_slot(time,kaiten){
+    setTimeout
+  }
+  // div1_write("Hello!", 2);
+  /*-この下からSlot Programを書いていく。*/
+  
+  
 }
