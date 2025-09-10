d9e8a13ae678e80f2f39d70d9058c296ad48a401
kansaisoka.ks562406 <ks562406@s.kansai.soka.ed.jp>
2025-09-10 10:57:47 +0900

    Update numberwrite.js

--- numberwrite.js
+++ numberwrite.js
@@ -1,7 +1,7 @@
 function slotwrite() {
   function div1_write(output, h_id) {
     if (h_id < 1 || h_id > 6) h_id = 6; // 範囲外なら h6 に
-    div1.innerHTML = "<h" + h_id + ">" + output + "</h" + h_id + ">";
+    div2.innerHTML = "<h" + h_id + ">" + output + "</h" + h_id + ">";
   }
   function speed_slot(time,kaitensuu){
     for (let i=0;i!=kaitensuu;i++){
