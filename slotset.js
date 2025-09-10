2c2c0ffee07ec73a959c28263c425f97dcb0ba69
kansaisoka.ks562406 <ks562406@s.kansai.soka.ed.jp>
2025-09-10 11:00:08 +0900

    Update slotset.js

--- slotset.js
+++ slotset.js
@@ -1,4 +1,5 @@
 function slotset(){
-  div1=document.getElementById('slotno_1')
-  div2=document.getElementById('comment_1')
+  div1=document.getElementById('head_text_1')
+  div2=document.getElementById('slotno_1')
+  div3=document.getElementById('comment_1')
 }
